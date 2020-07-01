ce4b886a583e4788e3849584b7a9647b505608be
WillCKyle <williamofnewe@gmail.com>
2020-07-01 10:02:43 -0500

    Merged the portfolio

---
+++ meta.js
@@ -0,0 +1,21 @@
+function myFunction() {
+  /* Get the text field */
+  var copyText = document.getElementById("Email");
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /*For mobile devices*/
+
+  /* Copy the text inside the text field */
+  document.execCommand("copy");
+
+  /* Alert the copied text */
+  alert("Copied the text: " + copyText.value);
+}
+var options = {
+    animateThreshold: 100,
+    scrollPollInterval: 50
+}
+$(document).ready(function(){
+  $('.aniview').AniView(options);
+});
